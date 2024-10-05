e48164b4f64cfcf46bb1ee7f809d8e6b63cff5ee
KuchikiZit <younes.efk.92@gmail.com>
2024-10-05 23:04:13 +0200

    connection SSH

--- public class HelloWorld {.java
+++ public class HelloWorld {.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World SABRI !!!");
+        System.out.println("J'ai emis une connection SSH !!!");
     }
 }
