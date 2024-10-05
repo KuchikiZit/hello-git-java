f6bba4573755a070fa304cfd65ab73f68963ba3a
KuchikiZit <younes.efk.92@gmail.com>
2024-10-05 22:53:54 +0200

    POUR SABRI !

--- public class HelloWorld {.java
+++ public class HelloWorld {.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World Younes!");
+        System.out.println("Hello, World SABRI !!!");
     }
 }
